0262e8ebeb3b0fc0f34d96366d7c781f0643e0d0
kubotako <kubotak143@gmail.com>
2025-02-26 21:05:08 +0900

    フィールドクリーチャーのクラスを作成した

--- backend/src/game.py
+++ backend/src/game.py
@@ -1,18 +1,51 @@
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
-# まだテストとかを書いてないので消しておく
+# playerが全てを持っている感じにする？
+# abilityはtargetに対して影響を与えるようにする
 #@dataclass
 #class Field:
 #  hand: list
 #  battlefield: list
-#  mana: list
+#  manafield: list
+#  library: list
 #
+#  @property
+#  def mainboard(self):
+#    return self.library.mainboard
+#
+#  @property
+#  def sideboaerd(self):
+#    return self.library.sideboard
+
+class FieldObject(ABC):
+  def __init__(self):
+    pass
+
+class FieldCreature(FieldObject):
+  def __init__(self, name, parameter, abilities):
+    self.name = name
+    self.params = parameter
+    self.abilities = abilities
+    self.summonsick = True
+
+# カードから先に定義すると訳わからなくなるのでフィールドオブジェクトから先に定義したほうが良さげ
+#class Card(ABC):
+#  def __init__(self, name, parameter, abilities):
+#    self.name = name
+#    self.parameter = parameter
+#    self.abilities = abilities
+#
+#  @abstractmethod
+#  def play(self):
+#    pass
+
 #class Player:
-#  def __init__(self, deck):
-#    self.library = deck.mainboard
+#  def __init__(self, life_point):
 #    self.field = Field()
+#    self.lp = life_point
 #
-#class Creature:
+#class CreatureCard(Card):
 #  def __init__(self, name, parameters, abilities):
 #    self.name = name
 #    self.parameter = parameter
@@ -24,3 +57,13 @@ class CreatureParameter:
   power: int
   toughness: int
   speed: int
+
+#class Ability(ABC):
+#  @abstractmethod
+#  def affect(self, target):
+#    pass
+#
+#class Haste(ability):
+#  def affect(self, field_creature):
+#    field_creature.summonsick = false
+#    return field_creature
--- backend/tests/game_test.py
+++ backend/tests/game_test.py
@@ -1,5 +1,12 @@
 import pytest
-from src.game import CreatureParameter
+from src.game import CreatureParameter, FieldCreature
+
+mock_goblin_parameter = CreatureParameter(1, 1 ,1 ,1)
+mock_field_goblin = FieldCreature(
+  name="Goblin",
+  parameter=mock_goblin_parameter,
+  abilities=[]
+)
 
 def test_parameter():
   cost, power, toughness, speed = 1, 1, 1, 1
@@ -9,3 +16,11 @@ def test_parameter():
   assert power == p.power
   assert toughness == p.toughness
   assert speed == p.speed
+
+def test_field_creature():
+  assert "Goblin" == mock_field_goblin.name
+  assert 1 == mock_field_goblin.params.cost
+  assert 1 == mock_field_goblin.params.power
+  assert 1 == mock_field_goblin.params.toughness
+  assert 1 == mock_field_goblin.params.speed
+  assert True == mock_field_goblin.summonsick
